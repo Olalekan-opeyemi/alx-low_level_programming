a6b2519e4bef84fc80db01a66206baae7d81eb19
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-04-21 17:06:30 -0700

    random number to the variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main - entry point
+ * Description: program will assign a random number to the variable n each
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+       	int F;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (F == 0)
+	{
+		printf("F of %d is %d and is 0\n", F, n);
+	}
+	else if (F > 5)
+	{
+		printf("F of %d is %d and is greater than 5\n, F, n"
+	}
+	else if (F > 0 < 6)
+	{
+	printf("F of %d is %d and is less than 6 and not 0\n", F, n);
+	}
+	return (0);
+}

58dc3b31f425e90abb7146f275fa4dbfddaf2df6
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-04-26 15:22:41 -0700

    function that checks for lowercase character.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /*
- * main - Entry point
- * print_alphabet_x10: print alphabets in small letters
- * multuple times.
+ * print_alphabet_x10 - Entry point.
+ * Description: print alphabets in small letters
+ * multiple times.
  * Return: 0
  */
 void print_alphabet_x10(void)
 
 {
 	char i;
-	int j = 0;	
-		while (j < 10)
+	int j = 0;
+
+	while (j < 10)
 	{
-		for (i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z';)
 		{
 		_putchar(i);
+		i++;
+		}
 		_putchar('\n');
 		j++;
-		}
 	}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * _islower - function that checks for lowercase character.
+ * c: the value to be checked
+ * Return: void
+ */
+int _islower(int c);
+{
+		if (c == 97 && c <= 122)
+		{
+			return (1);
+		}
+			return (0);
+}

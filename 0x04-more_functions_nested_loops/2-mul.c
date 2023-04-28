a4cc070949a56a45cb8d401935b2828e7c30b9f9
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-04-28 07:23:38 -0700

    prints multplication of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - prints the multiplication
+ * of two numbers
+ * @a: characters to check
+ * @b: characters to check
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

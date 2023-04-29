1e05cd4303ee63e8d61529bd4ee36a3f1b8bc9af
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-04-29 11:18:35 -0700

    prints fizz buzz for multiples of 3,5 and multiple of both 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,33 @@
  */
 int main(void)
 {
-	int i;
+	int i = 0, n = 0,  m = 0;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		n = i % 3;
+		m = i % 5;
+
+		if (n == 0 && m == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
+		else if (m == 0 && n != 0)
 		{
 			printf("Buzz");
+		}
+		else if (n == 0 && m != 0)
+		{
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%d", i);
 		}
-		printf(" ");
+		if (i < 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);

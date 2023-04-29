1787e5bfcdccb97514cea451ed4159e1db2c89fb
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-04-28 18:49:14 -0700

    print multioles of 3,5 and other number to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print multiples if 3,5
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

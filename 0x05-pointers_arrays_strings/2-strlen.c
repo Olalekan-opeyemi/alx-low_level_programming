d96c9ddf33a643ca0985852fd926b2b7640461d5
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-04 06:38:19 -0700

    prints the lenght of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,10 +10,14 @@ int _strlen(char *s)
 {
 	int l = 0;/*initialize variable l to act as a string counter*/
 
-	while (*s != '\0')/*enters a loop to continue as far as \0 is nit reachrd in the string*/
+	while (*s != '\0')/*enters a loop to continue as
+			    *far as \0 is nit reachrd in the string
+			    */
 	{
-		l++;	/*increment l in the loop*/
- 		s++;	/*increment s the pointer to point to tge next character of the string*/
+		l++;/*increment l in the loop*/
+		s++;/*increment s the pointer to point to
+		      *the next character of the string
+		      */
 	}
 	return (l);/*return the value of l which is the lenght of the string*/
 }

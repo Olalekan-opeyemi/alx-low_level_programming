e2e93bc828db89baca5a7f66d67f8c2bfcac2d43
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-04 06:28:27 -0700

    print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,9 +7,11 @@
  */
 void _puts(char *str)
 {
-	while (*str != '\0')	/*points to the string as long as the string isn't equal to the last digit /0*/
+	while (*str != '\0')/*points to the string as long as the 
+			      string isn't equal to the last digit /0*/
 	{
-		_putchar(*str++);	/*move pointer to next character as far as condition stays true*/
+		_putchar(*str++);/*move pointer to next character as far as
+				   condition stays true*/
 	}
 		_putchar('\n');	/*print a new line after string has been printed on stdout*/
 }

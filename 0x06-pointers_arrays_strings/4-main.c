9a5cbae09a37eb5394f3a55192f2aff01dfce3b9
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-09 14:54:19 -0700

    function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -34,7 +34,9 @@ int main(void)
 {
 	int a[] = {0 ,1 ,2 ,3 , 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	print_arrays(a, sizeof(a) / sizeof(int));
-	print_arrays(a, sizeof(a) / sizeof(int));
-	print_arrays(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+
+	return (0);
 }

eae2d26f2404a0dcce2225be3db6dec57f1b0762
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-12 05:10:39 -0700

    Prints the lenght if a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,13 @@
 /**
  *_strlen_recursion - Print the lenght of the string.
  *@s: pointer variable.
- *@i: counter variable for the string.
+ * Return: the lenght of string s
  */
-int _strlen_recursion(char* s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
 }

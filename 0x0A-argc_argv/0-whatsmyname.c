aab720f957c117f0a3a2f0395f37731c136db3a0
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-18 12:35:56 -0700

    Program that prints the program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
 #include <unistd.h>
-
+/**
+ *_putchar - Function
+ *@c: character to print
+ *Return: 1
+ */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 /**
  *main - Entry point
  *@argc: Argument count
  *@argv: Argument Vector
+ *Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	int i;
-	
-	i = 0;
 
+	i = 0;
 	while (argv[0][i] != '\0')
 	{
 		_putchar(argv[0][i]);

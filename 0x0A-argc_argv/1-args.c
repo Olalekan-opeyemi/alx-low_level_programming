564c2447596426028a9a9ec1216f5e24c3de2ebe
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-18 17:16:47 -0700

    Prints the argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: Argument count
+ *@argv: Argument vector
+ *Return: always success
+ *
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

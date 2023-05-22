cf9be59f180bba1cec523b9bbfa3aca1f44e3ad1
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-05-22 02:26:09 -0700

    program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main : Entry point.
- * @argv ; Argument Vector
+ * main - Entry point.
+ * @argv: Argument Vector
  * @argc: Argument Count
- * @Return: success
+ * Return: 1 if theres an error, 0 if it's successful or cents is negative.
  */
 int main(int argc, char **argv)
 {
 
 	int cents, change = 0;
-       
+
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -22,25 +22,29 @@ int main(int argc, char **argv)
 	{
 		printf("0\n");
 	}
-
-	change += cents/25;/*find how many times 25 can go in the value of cent and add the result to change*/
-	cents %= 25;/*find if theres any remainder in the value of cents using modulus 25*/
-
-	change += cents / 10;/*cents is now the remainder from the operation above , if any.*/
+	change += cents / 25;/*
+			* find how many times 25 can go in the value
+			*of cent and add the result to change
+			*/
+	cents %= 25;/*find if theres any remainder in the value of
+			*cents using modulus 25
+			*/
+	change += cents / 10;/*cents is now the remainder from
+				*the operation above , if any.
+				*/
 	cents %= 10;
 
 	change += cents / 5;
 	cents %= 5;
 
 	change += cents / 2;
-        cents %= 2;
+	cents %= 2;
 
 	change += cents / 1;
-        cents %= 1;
+	cents %= 1;
 
 	change += cents;
 
 	printf("%d\n", change);
-
 	return (0);
 }

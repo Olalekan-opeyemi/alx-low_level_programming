bd1d2279fc95a20d8faa4c684eb8769ed1823653
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-06-02 18:18:57 -0700

    Singly Linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *print_list - Function
+ *@list_t *h: data structure
+ *@h: pointer to first node
+ *Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+
+{
+	size_t count;
+
+	if (h->str == NULL)
+	{
+	printf("[0] (nil)\n");
+	}
+	for (count = 0; h != NULL; h = h->next)
+	{
+		printf("[%d] %s", h->len, h->str);
+		count++;
+	}
+	return (count);
+}

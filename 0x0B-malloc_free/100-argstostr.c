48335481f3c98cb78975702919530803f9eca685
Olalekan_opeyemi <olalekanopeyemi2005@gmail.com>
2023-06-06 13:52:39 -0700

    function that concatenates arguments and print each on a newline

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,43 @@
 #include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
+/**
+ *my_strlen - function that finds lenght of string
+ * @str: string
+ * Return: len
+ */
+size_t my_strlen(const char *str)
+{
+	size_t len = 0;
 
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *my_strcpy - Function that copies a string
+ * @src: holder of string
+ * @dest: holder of string
+ *Return: dest
+ */
+char *my_strcpy(char *dest, const char *src)
+{
+	int s = 0;
+
+	while (src[s] != '\0')
+	{
+	dest[s] = src[s];
+	s++;
+	}
+	return (dest);
+}
 /**
  *argstostr - Function
  *@ac: argument count
  *@av: argument vector
- *Return: pointer on success
+ *Return: pointer that concatenates argumenst
  */
 char *argstostr(int ac, char **av)
 {
